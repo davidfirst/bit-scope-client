8d06d1eb0479253589f493bd19ffce0ca1ff6f6b
Ran Yitzhaki <rany@cocycles.com>
2017-03-22 17:33:10 +0200

    remove index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const importer = require('./dist/main');
-const mockBitId = ['bit.utils/array/flat-map', 'bit.promise/global/promisify', 'bit.utils/array/diff']; // maybe add :: latest if needed
-importer(mockBitId).then((componentDependencies) => {
-  console.log(JSON.stringify(componentDependencies[0]));
-});
